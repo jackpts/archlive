3edba26275913e4e606bb81eba5136ba79f719ff
Jack Pts <jackpts1981@gmail.com>
2025-02-19 19:23:51 +0300

    yay installing fix

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -3,5 +3,10 @@ useradd -m -s /bin/fish jacky
 echo "jacky:123" | chpasswd
 systemctl enable NetworkManager
 
-pacman -S --noconfirm yay
+sudo pacman -S git base-devel
+cd $HOME/Downloads
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
 yay -S --noconfirm paru
